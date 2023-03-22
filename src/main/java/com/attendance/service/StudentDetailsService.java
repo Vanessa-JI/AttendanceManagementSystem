9e77f32922d2f4365a85b86e73ef6cc9e0af0cad
vanessa-ig <66692723+vanessa-ig@users.noreply.github.com>
2023-03-22 20:20:16 +0000

    Added the StudentDetailsService back to file directory V9 22/3, 20:20pm

---
+++ src/main/java/com/attendance/service/StudentDetailsService.java
@@ -0,0 +1,60 @@
+package com.attendance.service;
+
+import com.attendance.dao.StudentRowMapper;
+import com.attendance.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class StudentDetailsService implements UserDetailsService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentDetailsService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String sql = "SELECT studentUsername, firstName, lastName, password FROM student WHERE studentUsername = ?";
+        Student student = jdbcTemplate.queryForObject(sql, new Object[]{username},
+                (rs, rowNum) -> new Student(
+                        rs.getString("studentUsername"),
+                        rs.getString("firstName"),
+                        rs.getString("lastName"),
+                        rs.getString("password"),
+                        null)); // authorities will be set later
+        if (student == null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        String authoritySql = "SELECT authority FROM authorities WHERE studentUsername = ?";
+        List<GrantedAuthority> authorities = jdbcTemplate.query(authoritySql, new Object[]{username},
+                (rs, rowNum) -> new SimpleGrantedAuthority(rs.getString("authority")));
+        student.setAuthorities(authorities);
+        return student;
+    }
+
+    public Student authenticate(String username, String password) {
+        String sql = "SELECT * FROM student WHERE studentUsername = ?";
+        Student student = (Student) jdbcTemplate.queryForObject(sql, new Object[]{username}, new BeanPropertyRowMapper(Student.class));
+//        Student student = jdbcTemplate.queryForObject(sql, new Object[]{username}, new StudentRowMapper());
+
+        if (student != null && student.getPassword().equals(password)) {
+            return student;
+        } else {
+            return null;
+        }
+    }
+
+
+}
